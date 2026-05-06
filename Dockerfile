9e0e1a1ec106ef4a9991aece08960609f46b4b59
Wilson Yucra <wilsonyucra413@gmail.com>
2026-05-05 20:46:36 -0400

    puerto10000

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
-# Build
+# Etapa de construcción
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /src
+
+# Copiar el proyecto y restaurar
 COPY ["ConsultaExterna.csproj", "./"]
 RUN dotnet restore "./ConsultaExterna.csproj"
+
+# Copiar todo y publicar
 COPY . .
 RUN dotnet publish "ConsultaExterna.csproj" -c Release -o /app/publish
 
-# Runtime
+# Etapa de ejecución
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /app
 COPY --from=build /app/publish .
-ENV ASPNETCORE_URLS=http://0.0.0.0:8080
-EXPOSE 8080
+
+# Render usa el puerto 10000 por defecto
+ENV ASPNETCORE_URLS=http://+:10000
+EXPOSE 10000
+
 ENTRYPOINT ["dotnet", "ConsultaExterna.dll"]
--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using ConsultaExterna.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ConsultaExternaContext");
+var connectionString = builder.Configuration.GetConnectionString("ConsultaExternaContext") 
+    ?? Environment.GetEnvironmentVariable("ConnectionStrings__ConsultaExternaContext");
 
 builder.Services.AddDbContext<ConsultaExternaContext>(options =>
     options.UseNpgsql(connectionString));
@@ -14,7 +15,8 @@ builder.Services.AddSwaggerGen();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+// Cambia 8080 por 10000
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
 // Configure the HTTP request pipeline.
